c191c10cdacf332ba22d3ae5018e3dcf85832536
Watts-Blake <watts.blake52@gmail.com>
2023-08-09 13:07:27 -0700

    finished m1wk3d2 scratch notes

--- 1-Module/3-week/2-day/scratch.js
+++ 1-Module/3-week/2-day/scratch.js
@@ -53,16 +53,92 @@ let average = function (num1, num2) {
 
 // console.log(copy, person);
 
+// let person = {
+//   firstName: "Blake",
+//   lastName: "Watts",
+//   dog: "paisley",
+//   age: 28,
+//   hobbies: ["coding", "hiking", "video games"],
+// };
+
+// // keying in with bracket notation
+// // let namee = "firstName";
+// console.log(person.firstName);
+// console.log(person["firstName"]);
+// console.log(person[namee]);
+
+let car = {};
+
+let colorVar = "color";
+
+// car.colorVar = "blue";
+
+// car.color = "blue"; ✅
+
+// car[colorVar] = "blue"; ✅
+
+// car[0] = "blue";
+
+// console.log(car);
+// let cat = {};
+// let myKey = "color";
+
+// cat.myKey = "orange";
+// console.log(cat.myKey);
+
+// let person = {
+//   firstName: "Blake",
+//   lastName: "Watts",
+//   dog: "paisley",
+//   age: 28,
+//   hobbies: ["coding", "hiking", "video games"],
+// };
+// for (let i = 0; i < person.length; i++) {
+//   console.log("were running");
+// }
+// console.log(person.length);
+
+// for (let key in person) {
+//   console.log(key, person[key]);
+// }
+
+// let person = {
+//   firstName: "Blake",
+//   lastName: "Watts",
+//   dog: "paisley",
+//   age: 28,
+//   hobbies: ["coding", "hiking", "video games"],
+//   printName: (name) => console.log(name),
+// };
+
+// person.printName("edward");
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+let values = Object.values(person);
+
+// console.log(values);
+
+// for (let i = 0; i < values.length; i++) {
+//   let value = values[i];
+//   if (value === "Watts") {
+//     return console.log("we got it", i);
+//   }
+// }
+
 let person = {
   firstName: "Blake",
   lastName: "Watts",
   dog: "paisley",
   age: 28,
   hobbies: ["coding", "hiking", "video games"],
+  printName: (name) => console.log(name),
 };
 
-// keying in with bracket notation
-// let namee = "firstName";
-console.log(person.firstName);
-console.log(person["firstName"]);
-console.log(person[namee]);
+let hobbies = ["coding", "hiking", "video games"];
+
+for (let key in person) {
+  console.log(key);
+}
