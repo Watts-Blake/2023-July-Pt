870038a808fd18bf0bb464339e22a48a8d781b2b
tristan <thuckabee.work@gmail.com>
2023-11-01 15:46:49 -0500

    west setup

---
+++ 3-Module/3-week/3-day/scratch.js
@@ -0,0 +1,7 @@
+// 1. Import http
+
+// 2. Call 'createServer' passing in a callback that takes in the request and response
+
+// 3. Define the port to listen to
+
+// 4. Tell the server to listen for requests on the port
